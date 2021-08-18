027e8ad3f6d37837bf2e8fb62896212bc0370b89
NorbertoStudios <norbertostudios@gmail.com>
2021-08-18 01:01:11 -0400

    Initial Commit

---
+++ Basic Todo/Basic_TodoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Basic_TodoApp.swift
+//  Basic Todo
+//
+//  Created by Norberto Fernandez on 8/18/21.
+//
+
+import SwiftUI
+
+@main
+struct Basic_TodoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
